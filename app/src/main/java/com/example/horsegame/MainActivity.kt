066c02799f859714b255b3e070efb14afb7c5943
matias.norambuena <matias.norambuena@neolab.cl>
2023-12-10 01:18:28 -0300

    grid size screen

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.horsegame
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableRow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initScreenGame()
+    }
+
+    private fun initScreenGame() {
+        setSizeBoard()
+        hide_message()
+    }
+
+    private fun setSizeBoard() {
+        var iv: ImageView
+
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width = size.x
+
+        var width_dp = (width / resources.displayMetrics.density)
+
+        var lateralMarginsDP = 0
+        var width_cell = (width_dp - lateralMarginsDP)/8
+        val height_cell = width_cell
+
+        for (i in 0..7) {
+            for (j in 0..7) {
+                iv = findViewById(resources.getIdentifier("c$i$j", "id", packageName))
+
+                var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height_cell, resources.displayMetrics).toInt()
+                var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width_cell, resources.displayMetrics).toInt()
+
+                iv.layoutParams = TableRow.LayoutParams(width, height)
+            }
+        }
+    }
+
+    private fun hide_message() {
+        var lyMessage = findViewById<LinearLayout>(R.id.LyMessage)
+        lyMessage.visibility = View.INVISIBLE
     }
 }
